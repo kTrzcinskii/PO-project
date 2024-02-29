44e1fe43e77a1f6a6ad2b1dde2521bab7c311bb5
kTrzcinskii <kt.trzcinski@gmail.com>
2024-02-29 23:01:48 +0100

    Remove command line arguments

--- FlightManager/Program.cs
+++ FlightManager/Program.cs
@@ -1,18 +1,18 @@
 ﻿using FlightManager.DataParser;
 using FlightManager.DataSerializer;
-using FlightManager.ProgramArguments;
 
 namespace FlightManager;
 internal class Program
 {
     static void Main(string[] args)
     {
-        var arguments = ArgumentsParser.Parse(args);
+        var input = "example_data.ftr";
+        var output = "output.json";
         IDataParser parser = new FTRDataParser();
         IDataSerializer dataSerializer = new JSONDataSerializer();
 
         FlightManager flightManager = new FlightManager(parser, dataSerializer);
-        flightManager.LoadEntities(arguments.inputPath);
-        flightManager.SerializeEntities(arguments.outputPath);
+        flightManager.LoadEntitiesFromFile(input);
+        flightManager.SaveEntitiesToFile(output);
     }
 }

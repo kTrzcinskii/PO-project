2cf795ca53616ce097c6335a94c6e8ab41be7539
kTrzcinskii <kt.trzcinski@gmail.com>
2024-03-05 15:59:05 +0100

    Make some fields nullable due to requirements of second stage

--- FlightManager/Entity/Flight.cs
+++ FlightManager/Entity/Flight.cs
@@ -7,13 +7,14 @@ internal class Flight : IEntity
     public ulong TargetID { get; init; }
     public string TakeOffTime { get; init; }
     public string LandingTime { get; init; }
-    public float Longitude { get; init; }
-    public float Latitude { get; init; }
-    public float AMSL { get; init; }
+    public float? Longitude { get; init; }
+    public float? Latitude { get; init; }
+    public float? AMSL { get; init; }
     public ulong PlaneID { get; init; }
     public ulong[] CrewIDs { get; init; }
     public ulong[] LoadIDs { get; init; }
-    public Flight(ulong iD, ulong originID, ulong targetID, string takeOffTime, string landingTime, float longitude, float latitude, float aMSL, ulong planeID, ulong[] crewIDs, ulong[] loadIDs)
+
+    public Flight(ulong iD, ulong originID, ulong targetID, string takeOffTime, string landingTime, float? longitude, float? latitude, float? aMSL, ulong planeID, ulong[] crewIDs, ulong[] loadIDs)
     {
         ID = iD;
         OriginID = originID;

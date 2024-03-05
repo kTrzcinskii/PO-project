9105015445fa8034f2efc86ba8e0d95323e20e45
kTrzcinskii <kt.trzcinski@gmail.com>
2024-03-05 16:37:17 +0100

    Added converter for NetworkSourceSimulator data source

---
+++ FlightManager/DataParser/NetworkSourceSimulatorDataParser.cs
@@ -0,0 +1,33 @@
+﻿using FlightManager.Entity;
+using System.Text;
+
+namespace FlightManager.DataParser;
+internal class NetworkSourceSimulatorDataParser : IDataParser<byte[], byte[]>
+{
+    public (string, byte[]) ParseData(byte[] data)
+    {
+        const int entityCodeLength = 3;
+
+        using MemoryStream memStream = new MemoryStream(data);
+        using BinaryReader reader = new BinaryReader(memStream);
+
+        string entityName = MessageCodeToEntityIdentifier(Encoding.Default.GetString(reader.ReadBytes(entityCodeLength)));
+        uint messageLength = reader.ReadUInt32();
+        byte[] parameters = new byte[messageLength];
+        Array.Copy(data, memStream.Position, parameters, 0, messageLength);
+        return (entityName, parameters);
+    }
+
+    private static string MessageCodeToEntityIdentifier(string code) => code switch
+    {
+        "NCR" => EntitiesIdentifiers.CrewID,
+        "NPA" => EntitiesIdentifiers.PassengerID,
+        "NCA" => EntitiesIdentifiers.CargoID,
+        "NCP" => EntitiesIdentifiers.CargoPlaneID,
+        "NPP" => EntitiesIdentifiers.PassengerPlaneID,
+        "NAI" => EntitiesIdentifiers.AirportID,
+        "NFL" => EntitiesIdentifiers.FlightID,
+        _ => throw new ArgumentException("Invalid code")
+    };
+
+}

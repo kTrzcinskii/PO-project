e587a32ba5b1604ad88e45899d6eb6bda0afdf3c
kTrzcinskii <kt.trzcinski@gmail.com>
2024-03-24 14:37:41 +0100

    Add GUI logic to FlightManager

--- FlightManager/FlightManager.cs
+++ FlightManager/FlightManager.cs
@@ -1,6 +1,9 @@
 ﻿using FlightManager.DataLoader;
 using FlightManager.DataSerializer;
 using FlightManager.Entity;
+using FlightManager.GUI;
+using FlightTrackerGUI;
+using System.Timers;
 
 namespace FlightManager;
 internal class FlightManager
@@ -11,6 +14,7 @@ internal class FlightManager
     private readonly object entitiesLock = new object();
     private const string EXIT_COMMAND = "exit";
     private const string SNAPSHOT_COMMAND = "print";
+    private const int REFRESH_SCREEN_MS = 1000;
 
     public FlightManager(IDataLoader dataLoader, IDataSerializer dataSerializer)
     {
@@ -22,6 +26,7 @@ internal class FlightManager
     public void StartApp(string dataPath)
     {
         LoadEntities(dataPath);
+        RunGUI();
         HandleApplicationInput();
     }
 
@@ -68,5 +73,40 @@ internal class FlightManager
     {
         var jsonData = DataSerializer.SerializeData([.. Entities]);
         File.WriteAllText(outputPath, jsonData);
+    }
+
+    private void RunGUI()
+    {
+        Task.Run(Runner.Run);
+        StartUpdateScreen(REFRESH_SCREEN_MS);
+    }
+
+    private void StartUpdateScreen(int refreshTimeMs)
+    {
+        System.Timers.Timer timer = new();
+        timer.Interval = refreshTimeMs;
+        timer.Elapsed += OnUpdateScreen!;
+        timer.Start();
+    }
+
+    private void OnUpdateScreen(object _, ElapsedEventArgs __)
+    {
+        var visitor = new UpdateGUIVisitor();
+        lock (entitiesLock)
+        {
+            foreach (var entity in Entities)
+            {
+                entity.AcceptVisitor(visitor);
+            }
+        }
+        var list = new List<FlightGUI>();
+        foreach (var flight in visitor.Flights)
+        {
+            // Airport hasn't been loaded yet
+            if (!visitor.Airports.ContainsKey(flight.OriginID) || !visitor.Airports.ContainsKey(flight.TargetID))
+                continue;
+            list.Add(new FlightGUIAdapter(flight, visitor.Airports[flight.OriginID], visitor.Airports[flight.TargetID]));
+        }
+        Runner.UpdateGUI(new FlightsGUIData(list));
     }
 }

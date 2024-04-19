e52554066ff8ec87da89da2365dbda804d3088b5
kTrzcinskii <kt.trzcinski@gmail.com>
2024-04-19 18:19:39 +0200

    Add updatePath to cli arguments list

--- FlightManager/ProgramArguments/ArgumentsParser.cs
+++ FlightManager/ProgramArguments/ArgumentsParser.cs
@@ -3,9 +3,9 @@ internal static class ArgumentsParser
 {
     public static FlightManagerArguments Parse(string[] args)
     {
-        if (args.Length != 1)
+        if (args.Length != 1 && args.Length != 2)
         {
-            Console.WriteLine("[USAGE]: ./FlighManager.exe {input_path}");
+            Console.WriteLine("[USAGE]: ./FlighManager.exe {input_path} {update_path}(optional)");
             Environment.Exit(1);
         }
         string inputPath = args[0];
@@ -15,6 +15,17 @@ internal static class ArgumentsParser
             Environment.Exit(1);
         }
 
-        return new FlightManagerArguments(inputPath);
+        string? updatePath = null;
+        if (args.Length == 2)
+        {
+            updatePath = args[1];
+            if (!File.Exists(updatePath))
+            {
+                Console.WriteLine($"[ERROR]: File \"{updatePath}\" doesn't exist");
+                Environment.Exit(1);
+            }
+        }
+
+        return new FlightManagerArguments(inputPath, updatePath);
     }
 }
--- FlightManager/ProgramArguments/FlighManagerArguments.cs
+++ FlightManager/ProgramArguments/FlighManagerArguments.cs
@@ -2,9 +2,11 @@
 internal struct FlightManagerArguments
 {
     public readonly string inputPath;
+    public readonly string? updatePath;
 
-    public FlightManagerArguments(string inputPath)
+    public FlightManagerArguments(string inputPath, string? updatePath = null)
     {
         this.inputPath = inputPath;
+        this.updatePath = updatePath;
     }
 }

dc473ec0b0a35249db6a7a27b26851706a5c42f3
kTrzcinskii <kt.trzcinski@gmail.com>
2024-05-13 14:50:06 +0200

    Make `QueryCondition` work with `IComparable` instead of `IComparable<T>`

--- FlightManager/Query/QueryCondition.cs
+++ FlightManager/Query/QueryCondition.cs
@@ -6,21 +6,20 @@ public struct QueryCondition(string property, object value, QueryConditionType t
     private readonly object _value = value;
     private readonly QueryConditionType _type = type;
 
-    public bool Check<T>(IComparable<T> property)
+    public bool Check(IComparable property)
     {
-        T compValue = (T)_value;
         switch (_type)
         {
             case QueryConditionType.EQ:
-                return property.CompareTo(compValue) == 0;
+                return property.CompareTo(_value) == 0;
             case QueryConditionType.GT:
-                return property.CompareTo(compValue) > 0;
+                return property.CompareTo(_value) > 0;
             case QueryConditionType.LT:
-                return property.CompareTo(compValue) < 0;
+                return property.CompareTo(_value) < 0;
             case QueryConditionType.GTE:
-                return property.CompareTo(compValue) >= 0;
+                return property.CompareTo(_value) >= 0;
             case QueryConditionType.LTE:
-                return property.CompareTo(compValue) <= 0;
+                return property.CompareTo(_value) <= 0;
         }
         return false;
     }

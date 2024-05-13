a1f20e4f1c68e59d5c05d7719705840f1eea7a1c
kTrzcinskii <kt.trzcinski@gmail.com>
2024-05-13 14:50:40 +0200

    Implement getting all columns in `DisplayQuery`

--- FlightManager/Query/DisplayQuery.cs
+++ FlightManager/Query/DisplayQuery.cs
@@ -4,11 +4,46 @@ namespace FlightManager.Query;
 
 internal class DisplayQuery<T> : FilterableQuery<T> where T : IEntity
 {
-    public DisplayQuery(List<QueryCondition>? conditions, List<T> entities) : base(conditions, entities)
+    private List<string>? _fields;
+    
+    public DisplayQuery(List<QueryCondition>? conditions, List<T> entities, List<string>? fields) : base(conditions, entities)
     {
+        _fields = fields;
     }
+    
+    private Dictionary<string, (List<object> rows, int requiredColumnWidth)> PrepareColumns(List<T> data)
+    {
+        if (_fields == null)
+        {
+            _fields = T.GetAllFieldsNames();
+        }
 
-    private void PrintData(List<T> data)
+        var dict = new Dictionary<string, (List<object> rows, int requiredColumnWidth)>();
+        
+        foreach (var field in _fields)
+        {
+            dict.Add(field, (new List<object>(), 0));
+        }
+
+        foreach (var entity in data)
+        {
+            foreach (var field in _fields)
+            {
+                object val = entity.GetFieldValue(field);
+                dict[field].rows.Add(val);
+                int requiredLength = val.ToString()!.Length;
+                if (dict[field].requiredColumnWidth < requiredLength)
+                {
+                    var updated = (dict[field].rows, requiredLength);
+                    dict[field] = updated;
+                }
+            }
+        }
+
+        return dict;
+    }
+    
+    private void PrintData(Dictionary<string, (List<object> rows, int requiredColumnWidth)> columns)
     {
         Console.WriteLine("XD");
     }
@@ -16,7 +51,8 @@ internal class DisplayQuery<T> : FilterableQuery<T> where T : IEntity
     public override void Execute()
     {
         var data = FilterData();
-        PrintData(data);
+        var columns = PrepareColumns(data);
+        PrintData(columns);
     }
 
 }

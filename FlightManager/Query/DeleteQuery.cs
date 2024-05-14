1d412accab9b879042e7518bceebf422e7803002
kTrzcinskii <kt.trzcinski@gmail.com>
2024-05-14 13:47:42 +0200

    Implement `DeleteQuery`

--- FlightManager/Query/DeleteQuery.cs
+++ FlightManager/Query/DeleteQuery.cs
@@ -4,13 +4,25 @@ namespace FlightManager.Query;
 
 internal class DeleteQuery<T> : FilterableQuery<T> where T : IEntity
 {
+    private DeletingVisitor visitor;
+    
     public DeleteQuery(List<QueryCondition>? conditions, List<T> entities) : base(conditions, entities)
     {
+        visitor = new DeletingVisitor();
+    }
+
+    private void DeleteAll(List<T> entities)
+    {
+        foreach (var entity in entities)
+        {
+            entity.AcceptVisitor(visitor);
+        }
     }
     
     public override void Execute()
     {
-        throw new NotImplementedException();
+        var data = FilterData();
+        DeleteAll(data);
     }
 
 }
---
+++ FlightManager/Query/DeletingVisitor.cs
@@ -0,0 +1,49 @@
+﻿using FlightManager.Entity;
+using FlightManager.Storage;
+
+namespace FlightManager.Query;
+
+internal class DeletingVisitor : IEntityVisitor
+{
+    private EntityStorage _storage;
+    
+    public DeletingVisitor()
+    {
+        _storage = EntityStorage.GetStorage();
+    }
+    
+    public void VisitAirport(Airport airport)
+    {
+        _storage.RemoveAirport(airport.ID);
+    }
+
+    public void VisitCargo(Cargo cargo)
+    {
+        _storage.RemoveCargo(cargo.ID);
+    }
+
+    public void VisitCargoPlane(CargoPlane cargoPlane)
+    {
+        _storage.RemoveCargoPlane(cargoPlane.ID);
+    }
+
+    public void VisitCrew(Crew crew)
+    {
+        _storage.RemoveCrew(crew.ID);
+    }
+
+    public void VisitFlight(Flight flight)
+    {
+        _storage.RemoveFlight(flight.ID);
+    }
+
+    public void VisitPassenger(Passenger passenger)
+    {
+        _storage.RemovePassenger(passenger.ID);
+    }
+
+    public void VisitPassengerPlane(PassengerPlane plane)
+    {
+        _storage.RemovePassengerPlane(plane.ID);
+    }
+}

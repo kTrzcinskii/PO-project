a82cc599cb114299cbe7b78adee3d0d1f9c4c3c5
kTrzcinskii <kt.trzcinski@gmail.com>
2024-05-19 11:00:19 +0200

    Make fields nullable

--- FlightManager/Entity/WorldPosition.cs
+++ FlightManager/Entity/WorldPosition.cs
@@ -2,14 +2,14 @@
 
 public struct WorldPosition : IComparable
 {
-    public WorldPosition(float l, float lat)
+    public WorldPosition(float? l, float? lat)
     {
         Long = l;
         Lat = lat;
     }
 
-    public float Long { get; set; }
-    public float Lat { get; set; }
+    public float? Long { get; set; }
+    public float? Lat { get; set; }
     public override string ToString()
     {
         return $"{{{Long}; {Lat}}}";
@@ -17,12 +17,14 @@ public struct WorldPosition : IComparable
 
     public int CompareTo(object? obj)
     {
+        if (Long == null || Lat == null)
+            return 1;
         if (obj is WorldPosition)
         {
             WorldPosition wp = (WorldPosition)obj;
-            if (Long.CompareTo(wp.Long) != 0)
-                return Long.CompareTo(wp.Long);
-            return Lat.CompareTo(wp.Lat);
+            if (Long.Value.CompareTo(wp.Long) != 0)
+                return Long.Value.CompareTo(wp.Long);
+            return Lat.Value.CompareTo(wp.Lat);
         }
         return 1;
     }
